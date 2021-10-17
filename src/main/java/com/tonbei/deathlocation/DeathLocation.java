bffb52b8c7f3ed06e39e991b4a3cb3a16c9c2706
tonbei10 <47322051+tonbei10@users.noreply.github.com>
2021-10-17 17:04:04 +0900

    Initial commit

---
+++ src/main/java/com/tonbei/deathlocation/DeathLocation.java
@@ -0,0 +1,46 @@
+package com.tonbei.deathlocation;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathLocation extends JavaPlugin implements Listener {
+
+    private final boolean debug = false;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player deathplayer = e.getEntity();
+        Location location = deathplayer.getLocation();
+        double x = Math.round(location.getX() * 1000.0) / 1000.0;
+        double y = Math.round(location.getY() * 1000.0) / 1000.0;
+        double z = Math.round(location.getZ() * 1000.0) / 1000.0;
+        World.Environment dimension = deathplayer.getWorld().getEnvironment();
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            p.sendMessage("[" + ChatColor.RED + "Death Location" + ChatColor.RESET + "] <" + deathplayer.getDisplayName()
+                    + "> Dimension: " + ChatColor.DARK_PURPLE + dimension + ChatColor.RESET);
+            p.sendMessage("[" + ChatColor.RED + "Death Location" + ChatColor.RESET + "] "
+                    + "{ X: " + ChatColor.GREEN + x + ChatColor.RESET
+                    + ", Y: " + ChatColor.GREEN + y + ChatColor.RESET
+                    + ", Z: " + ChatColor.GREEN + z + ChatColor.RESET + " }");
+        }
+    }
+}
